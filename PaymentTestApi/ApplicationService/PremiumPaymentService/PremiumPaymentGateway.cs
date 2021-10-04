b87ca3ce6270c5086d36dcd169f0ba7f35651a42
Joseph Ikpontu <Jikpontu@gmail.com>
2021-10-04 21:08:45 +0100

    update

--- PaymentTestApi/ApplicationService/PremiumPaymentService/PremiumPaymentGateway.cs
+++ PaymentTestApi/ApplicationService/PremiumPaymentService/PremiumPaymentGateway.cs
@@ -1,4 +1,4 @@
-﻿using EasyRetry;
+﻿//using EasyRetry;
 using PaymentTestApi.ApplicationService.ProcessPayment;
 using PaymentTestApi.Models;
 using System;
@@ -11,22 +11,25 @@ namespace PaymentTestApi.ApplicationService.PremiumPaymentService
     public class PremiumPaymentGateway : IPremiumPaymentGateway
     {
         private readonly IProcessPaymentService _processPaymentService;
-        private readonly IEasyRetry _easyRetry;
-        public PremiumPaymentGateway(IProcessPaymentService processPaymentService, IEasyRetry _easyRetry)
+        //private readonly IEasyRetry _easyRetry;
+        public PremiumPaymentGateway(IProcessPaymentService processPaymentService /*,IEasyRetry easyRetry*/)
         {
             _processPaymentService = processPaymentService;
+            //_easyRetry = easyRetry;
         }
 
         public string GatewayName = "Premium Payment Gateway";
 
         public async Task<bool> ProcessPayment(PaymentRequest paymentRequest)
         {
-            var result = await _easyRetry.Retry(async () => await _processPaymentService.ProcessPayment(paymentRequest, GatewayName), new RetryOptions()
-            {
-                Attempts = 3,
-            });
+            //var result = await _easyRetry.Retry(async () => await _processPaymentService.ProcessPayment(paymentRequest, GatewayName), new RetryOptions()
+            //{
+            //    Attempts = 3,
+            //});
 
-            return result;
+            //await new EasyRetry().Retry(async () => await _processPaymentService.ProcessPayment(paymentRequest, GatewayName);
+
+            return false;
 
         }
     }
